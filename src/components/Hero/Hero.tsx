c501f8f0f948864f7fae08099c36a92524865e9c
Syanda-Mabaso <syandamabaso39@gmail.com>
2026-07-08 16:10:24 +0200

    ---------

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
-import styles from "./Hero.module.css"
+import './Hero.module.css'
+import sectionImage from '../../assets/images/herosection.png'
+// import styles from  './Hero.module.css'
 
-export const Hero = () => {
+function Hero(){
   return (
-    <div>Hero</div>
+    <>
+      <div >
+      <div style={{backgroundImage:`url(${sectionImage})`,
+      minHeight:'40vh', 
+      backgroundSize:'cover', 
+      backgroundPosition:'center',
+      backgroundRepeat:'no-repeat'
+      }}>     
+      <div className="hero-content">
+       <h1 className="hero-title"> Delight in</h1>
+       <p className="hero-p">PARTICU IN FUERY BITE!</p>
+       <button className="hero-btn">ORDER now</button>
+       </div>
+      </div>
+      </div>
+      </>
   )
 }
+ export default Hero;
