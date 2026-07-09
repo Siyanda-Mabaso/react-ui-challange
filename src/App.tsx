5f08cdaadbaea7f5f8e689f398db7e4db0423db3
Syanda-Mabaso <syandamabaso39@gmail.com>
2026-07-09 15:50:11 +0200

    created cards components

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import "./App.css";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import {Images} from "./components/Images/Images";
+import Card from "./components/Cards/Card"
 
 function App (){
    return(
       <>
         <Navbar />
        <Hero/>
-       <Images/>     
+       <Images/>   
+       <Card/>  
       </>
    )
 }
---
+++ src/components/Cards/Card.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import style from './Card.module.css'
+
+const Card = () => {
+  return (
+    <div>
+        <div>
+            <h3>Custom Cake</h3>
+            <p></p>
+            <button>Custom</button>
+        </div>
+    </div>
+  )
+}
+export default Card
